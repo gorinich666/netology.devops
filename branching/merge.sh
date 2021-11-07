325ccd5f5b3ef5adb7268bdc578150d42bd1c0fa
Vyacheslav Goryainov <vogoryainov@gmail.com>
2021-11-07 19:21:19 +0700

    merge: @ instead *

--- branching/merge.sh
+++ branching/merge.sh
@@ -2,7 +2,7 @@
 # display comand line options
 
 count=1
-for param in "$*"; do
-	echo "\$* Parametr #$count = $param"
+for param in "$@"; do
+	echo "\$@ Parametr #$count = $param"
 	count=$(( $count + 1 ))
 done
